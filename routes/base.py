73a64a1e3bdd9ca52b2c2a750a518cdc7c27ab73
Aymanalaqrabawi <alqrabawiayman@gmail.com>
2026-02-12 15:13:54 +0300

    new file:   routes/base.py

--- routes/base.py
+++ routes/base.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI
-from routes import base
-app = FastAPI()
-app.include_router(base.base)
-@app.get("/")
-def read_root():   
+from fastapi import FastAPI,APIRouter
+base = APIRouter()
+@base.get("/")
+def welcome():   
      return {"Hello": "World"}
 
 #uvicorn main:app --reload --host 0.0.0.0 --port 5000  where main is the name of the file and app is the name of the FastAPI instance.
+
 
